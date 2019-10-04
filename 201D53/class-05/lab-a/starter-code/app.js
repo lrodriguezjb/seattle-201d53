8a3930a8e3fe9ae329897b554d92d4bc72f60ef0
Leandro Rodriguez <lrodriguezjb@gmail.com>
2019-10-04 14:35:12 -0700

    added array multiplication function

--- 201D53/class-05/lab-a/starter-code/app.js
+++ 201D53/class-05/lab-a/starter-code/app.js
@@ -112,11 +112,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    var multthree = multiply(testArray[0], testArray[1]);
+    multthree = parseInt(multthree);
+    var multfour = multiply(multthree, testArray[2])
+    multfour = parseInt(multfour);
+    var finalresulttwo = ('The numbers ' + testArray[0] + ','+ testArray[1] + ',' + testArray[2] + ' have a product of ' + multfour + '.');
+    return[multfour, finalresulttwo];
+
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
